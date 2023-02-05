7875ff0ae0051e0cb9a7d99b307116c6d96c1cb2
maxlieb <maxzemail@gmail.com>
2023-02-05 20:50:44 +0200

    Update MulRDP.bat

--- MulRDP.bat
+++ MulRDP.bat
@@ -1,10 +1,3 @@
 @echo off
-::mkdir "\\%1\c$\Program Files\RDP Wrapper\"
-::copy "c:\users\m21372\OneDrive - Netafim\Tools\RDPWrap\*" "\\%1\c$\Program Files\RDP Wrapper\" /Y
-::psexec -s \\%1 "C:\Program Files\RDP Wrapper\autoupdate.bat"
-::echo Install done, have your session and when done return to this window, press enter to uninstall.
-::echo (quit this window now if you don't want to uninstall).
-::pause
-::psexec -s \\%1 "C:\Program Files\RDP Wrapper\uninstall.bat"
 powershell -ep bypass -noprofile -nologo -file "%~dp0MulRDP.ps1" %*
 pause
